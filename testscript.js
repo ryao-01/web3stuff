0e7c2d7ac8275aae9be4abcd7b545bac0624c997
Richard <richard.yao@antithesis.com>
2025-02-18 17:53:26 -0500

    Update: increased gas use

--- testscript.js
+++ testscript.js
@@ -8,13 +8,12 @@ if (!rpcUrl) {
 }
 
 const web3 = new Web3(rpcUrl);
-
 const privateKey = "0x12d7de8621a77640c9241b2595ba78ce443d05e94090365ab3bb5e19df82c625";
 const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 const fromAddress = account.address;
 
 const toAddress = fromAddress; 
-const valueWei = web3.utils.toHex('10000000000000000'); // 0.01 MATIC in Wei (hex)
+const valueWei = web3.utils.toHex('1000000'); 
 let nonceHex;
 let chainIdHex;
 
