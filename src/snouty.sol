a1fce7fc2924065082d2761ce15f3afdc2675577
Richard <richard.yao@antithesis.com>
2025-05-13 10:26:31 -0400

    FIX: older openzepplin ver changed how constuctor worked for erc20

--- src/snouty.sol
+++ src/snouty.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract SnoutyCoin is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("Snouty", "SNTY") Ownable(msg.sender) {
+
+    constructor(uint256 initialSupply) ERC20("Snouty", "SNTY") {
         _mint(msg.sender, initialSupply);
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
+    
 }
